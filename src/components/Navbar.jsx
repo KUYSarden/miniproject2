11fb90b8dda27de2ef28554bbbd952827e9b79fb
KUYSarden <ardenflores0@gmail.com>
2023-03-31 12:44:31 +0800

    added-logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import Button from 'react-bootstrap/Button';
+
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Blog from '../assets/Blog.png'
 
 
 const Navbars = () => {
@@ -11,7 +12,7 @@ const Navbars = () => {
    <>
     <Navbar bg="light" expand="lg">
     <Container fluid>
-      <Navbar.Brand href="#">Blog logo</Navbar.Brand>
+      <Navbar.Brand href="#"><span><img className='logo' src={Blog} alt="" /></span></Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarScroll" />
       <Navbar.Collapse id="navbarScroll">
         <Nav
@@ -36,14 +37,10 @@ const Navbars = () => {
             </NavDropdown>
               
         </Nav>
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
+        <Form className="d-flex top-nav-links">
+        <a className='log-in' href="">Log in</a>
+        <a className='sign-up' href="">Sign Up</a>
+
         </Form>
       </Navbar.Collapse>
     </Container>
